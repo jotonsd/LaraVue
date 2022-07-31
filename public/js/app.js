05119567c44843f9d958ca8e1a81dcac988dc526
Joton Sutradhar <jotonsutradharjoy@gmail.com>
2022-07-31 16:47:40 +0600

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -49474,8 +49474,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\My Works(Joton)\Projects\Laravel\Laravel5.7\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\My Works(Joton)\Projects\Laravel\Laravel5.7\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\My Works(Joton)\Githubs\LaraVue\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\My Works(Joton)\Githubs\LaraVue\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
